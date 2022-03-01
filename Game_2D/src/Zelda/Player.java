0f2c3b6de255e82ba5834afce29471193487cf4d
João Vitor <84301172+JVOA02@users.noreply.github.com>
2022-02-28 21:01:03 -0400

    Add files via upload

---
+++ Game_2D/src/Zelda/Player.java
@@ -0,0 +1,33 @@
+package Zelda;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Player extends Rectangle{
+	public int spd = 4;
+	public boolean right, up, down, left;
+	public Player(int x, int y) {
+		super(x,y,32,32);
+	}
+	
+	public void tick(){
+		if(right && World.isFree(x+spd, y)) {
+			x+=spd;
+			
+		}else if(left && World.isFree(x-spd, y)) {
+			x-=spd;
+		}
+		if(up && World.isFree(x, y-spd)) {
+			y-=spd;
+			
+		}else if(down && World.isFree(x, y+spd)) {
+			y+=spd;
+		}
+	}
+	
+	public void render(Graphics g){
+		g.setColor(Color.blue);
+		g.fillRect(x, y, width, height);
+	}
+}
